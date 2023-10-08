114e04a026db1d3acb6c85599c42b4b0860213c6
oc9x97 <142158555+oc9x97@users.noreply.github.com>
2023-10-08 13:56:12 -0400

    Update discord.js

--- discord.js
+++ discord.js
@@ -35,6 +35,11 @@ class Discord {
                             defaultValue: 'Hello, Scratch!'
                         }
                     }
+                },
+                {
+                    opcode: 'deleteWebhook',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'Delete Webhook',
                 }
             ]
         };
@@ -72,6 +77,26 @@ class Discord {
             if (!response.ok) {
                 throw new Error(`Failed to send webhook message: ${response.statusText}`);
             }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }
+
+    deleteWebhook() {
+        if (!this.webhookURL) {
+            throw new Error('Webhook URL is not set. Please use the "Set webhook URL" block to set it.');
+        }
+
+        // Use the fetch method with the DELETE HTTP method to delete the webhook
+        fetch(this.webhookURL, {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to delete webhook: ${response.statusText}`);
+            }
+            this.webhookURL = ''; // Reset the webhook URL
         })
         .catch(error => {
             console.error(error);
